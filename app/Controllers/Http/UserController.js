7b25902dfe6c44b9a84a174df11db52afce49d11
Deyvison Rocha <deyvison@gmail.com>
2019-11-24 22:26:13 -0300

    update controller and fix methods

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -21,18 +21,48 @@ class UserController {
     return view.render('users.create', { sectionTitle: 'User - Create' })
   }
 
-  async store ({ request, response }) {
-    const data = request.only(['username', 'email', 'password'])
+  async store ({ request, response, session }) {
+    await User.create(request.only(['username', 'email', 'password']))
 
-    await User.create(data)
+    session.flash({ successMessage: 'User has been created!' })
 
-    return response.route('users.index', { sectionTitle: 'Users' })
+    return response.route('users.index')
   }
 
   async edit({ view, params }) {
-    let user = await User.find(~~params.id)
+    const user = await User.find(~~params.id)
 
     return view.render('users.edit', { user: user.toJSON(), sectionTitle: 'User - Edit' })
+  }
+
+  async update({ request, response, params, session }) {
+    const user = await User.find(~~params.id)
+
+    const { username, password, email } = request
+      .only(['username', 'password', 'email'])
+
+    user.username = username
+    user.email = email
+
+    if (password) {
+      user.password = password
+    }
+
+    await user.save()
+
+    session.flash({ successMessage: 'User has been updated!' })
+
+    return response.route('users.index', { sectionTitle: 'User s' })
+  }
+
+  async destroy({ response, params, session }) {
+    const user = await User.find(~~params.id)
+
+    await user.delete()
+
+    session.flash({ successMessage: 'User has been removed!' })
+
+    return response.route('users.index')
   }
 }
 
